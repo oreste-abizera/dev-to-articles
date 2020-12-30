150650f63cf8679e283f186bf51d9de68f9b1875
oreste-abizera <oresteabizera11@gmail.com>
2020-12-30 19:54:40 +0200

    Project Functionality finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [articles, setarticles] = React.useState([]);
+  React.useEffect(() => {
+    fetch("https://dev.to/api/articles?username=oreste")
+      .then((res) => res.json())
+      .then((res) => {
+        setarticles(res);
+      });
+  }, []);
+
+  console.log(articles);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="articles">
+        {articles.map((article) => {
+          return (
+            <div key={article.id} className="article">
+              <a href={article.url} className="title">
+                {article.title}
+              </a>
+              <img src={article.social_image} alt={article.title}></img>
+              <p>
+                {article.description} <a href={article.url}>Read More.</a>
+              </p>
+              <p>
+                {article.readable_publish_date} | {article.tags}{" "}
+              </p>
+              <p> {article.public_reactions_count} reactions</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
